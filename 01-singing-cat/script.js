f2f8b00f650197382e6ce3f551b639a9dbcd1275
SEULGI YOON <sgyoon.info@gmail.com>
2019-10-25 18:50:26 +0900

    인덱스 페이지 및 프로젝트 01번 파일 업로드

---
+++ 01-singing-cat/script.js
@@ -0,0 +1,53 @@
+const blinkEyeRandomly = () => {
+  const openEye = document.querySelector(`img[data-key="open"]`);
+  const closeEye = document.querySelector(`img[data-key="close"]`);
+  openEye.style.display = 'none';
+  closeEye.style.display = 'block';
+
+  setTimeout(() => {
+    openEye.style.display = 'block';
+    closeEye.style.display = 'none';
+  }, 200);
+
+  setTimeout(blinkEyeRandomly, Math.floor(Math.random() * 8000));
+}
+
+const removeClassTransitionEnd = (selector, className) => {
+  const node = document.querySelectorAll(selector);
+
+  node.forEach(element => {
+    element.addEventListener('transitionend', function(){
+      element.classList.remove(className)});
+  });
+}
+
+const playSound = (event) => {
+  const sound = document.querySelector(`audio[data-key="${event.key}"]`);
+  if (!sound) { return; }
+  sound.currentTime = 0;
+  sound.play();
+}
+
+const underlineText = (event) => {
+  const text = document.querySelector(`div[data-key="${event.key}"]`);
+  if (!text) { return; }
+  text.classList.add('underline');
+}
+
+const changeMouth = () => {
+  let count = 0;
+
+  return function () {
+    count = count + 1;
+
+    const mouth = document.querySelector(`img[data-key="${arguments[0].key}"]`);
+    if (!mouth) { return; }
+    mouth.style.zIndex = count;
+  }
+}
+
+removeClassTransitionEnd('.key', 'underline');
+blinkEyeRandomly();
+window.addEventListener('keydown', playSound);
+window.addEventListener('keydown', underlineText);
+window.addEventListener('keydown', changeMouth(event));
