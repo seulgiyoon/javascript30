2f2426b38885e85b143ede23e5e66f888f304886
SEULGI YOON <sgyoon.info@gmail.com>
2019-11-03 20:37:56 +0900

    페이지 전체 디자인 및 mousemove이벤트시에도 blur값이 변하도록 적용

--- 02-pick-and-clear/script.js
+++ 02-pick-and-clear/script.js
@@ -1,16 +1,27 @@
 const pictures = document.querySelectorAll('.photo');
+const controlBar = document.querySelector('#blur');
+
+const setBlurBarZero = () => {
+  controlBar.value = 0;
+  document.documentElement.style.setProperty(`--${controlBar.name}`, '30px');
+}
+
 const toggleOpen = function() {
+  setBlurBarZero();  
   this.classList.toggle('open');
-  this.classList.toggle('selected-img');
 }
 
 pictures.forEach(photo => photo.addEventListener('click', toggleOpen));
 
-const controlBar = document.querySelector('#blur');
+
 const changeValue = function () {
   const selectedPic = document.getElementsByClassName('open');
-  if (!selectedPic) { return; }
-  document.documentElement.style.setProperty(`--${this.name}`, (30 - this.value) + 'px');
+  if (selectedPic.length === 1) {
+    document.documentElement.style.setProperty(`--${this.name}`, (30 - this.value) + 'px');
+  } else {
+    return;
+  }
 }
 
 controlBar.addEventListener('change', changeValue);
+controlBar.addEventListener('mousemove', changeValue);
